ebc51c9e4206436c834cccc1ea149a3d12884a3c
dezhizhang <1541609448@qq.com>
2022-07-18 04:45:57 +0800

    feat:c预编译

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,17 @@
+/*
+ * :file description: 
+ * :name: /vite/src/main.jsx
+ * :author: 张德志
+ * :copyright: (c) 2022, Tungee
+ * :date created: 2022-06-19 16:40:28
+ * :last editor: 张德志
+ * :date last edited: 2022-07-18 04:43:28
+ */
 import { createRoot } from 'react-dom/client';
 
 
+// const root = createRoot(document.getElementById('app'));
+// root.render(<h1>hello</h1>);
+
 const root = createRoot(document.getElementById('app'));
-root.render(<h1>hello</h1>);
+root.render(<h3>hello</h3>)
